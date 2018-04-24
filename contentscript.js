16f366640a6f68f803ef69d40207081280122f9a
xm0625 <402276694@qq.com>
2018-04-24 16:14:53 +0800

    initial commit

---
+++ contentscript.js
@@ -0,0 +1,62 @@
+chrome.extension.sendRequest({method: "get_rules"}, function(response) {
+    if (response.rules == "") {
+        // No rules, don't do anything!
+        return;
+    }
+
+    var rules = "";
+    var match = [];
+    rules = response.rules.split("\n");
+
+    // Loop through all of the rules and add the specific rule if there is a match
+    // If multiple rules match, we will take action on the last rule matched.
+    // This is so that we can override previous rules.
+    for (key in rules) {
+        var thisrule = rules[key].split(",");
+        var url_regex = thisrule[1].trim();
+        url_regex = url_regex.replace(/\*/g, "[^ ]+");
+        if (url_regex.indexOf("file://") == -1 && url_regex.indexOf("http") == -1) {
+            url_regex = "^http[s]?://" + url_regex;
+        }
+        if (window.location.pathname == "/") {
+            url_regex = url_regex + "[/]";
+        }
+        regexp = new RegExp(url_regex);
+        if (regexp.test(window.location.href)) {
+            // Set the match even if it's a revert operation
+            match = thisrule;
+        }
+    }
+
+    // If there is a match, process it
+    if (match[0] && chrome.extension.inIncognitoContext) {
+        var operation = match[0].trim().toLowerCase();
+        if (operation != "revert") {
+            var textval = "";
+            if (typeof match[2] != "undefined") {
+                textval = match[2].trim();
+            }
+
+            if (operation == "rename") {
+                document.title = textval;
+                setInterval(function(){
+                    if(document.title !== textval){
+                        document.title = textval;
+                    }
+                }, 200);
+            }
+
+            if (operation == "prefix") {
+                document.title = textval + " " + document.title;
+            }
+
+            if (operation == "suffix") {
+                document.title = document.title + " " + textval;
+            }
+
+            // Tell the background page that there is a match.
+            // This will make the icon appear in the address bar.
+            chrome.extension.sendRequest({method: "is_active"}, function(response) {});
+        }
+    }
+});
